8538cc5096a7461d6b42dd7fa26f9ad9af7e7084
Genaldo44 <lopesgenaldo@gmail.com>
2022-05-25 00:44:08 +0000

    aula 2

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = "banco.db"
+SECRET_KEY = "chave"
 
 app = Flask("Hello")
-@app.route("/hello")
-def hello():
-    return render_template("hello.html")  #primeiro app criado
+app.config.from_object(__name__)
+
+def conecta_bd():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conecta_bd()
+
+@app.teardown_request
+def depois_requisicao(e):
+    g.bd.close()
+
+@app.route("/")
+def exibir_entradas():
+    sql = "SELECT titulo, texto, criado_em FROM entradas ORDER BY id DESC"
+    cur = g.bd.execute(sql)
+    post = {"titulo": "Meu Titulo", "texto": "Olá"},
+    return  render_template("layout.html", post=post)
 
    
